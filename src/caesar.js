d89e699a879f4e00d70204eef8e6602620d985e0
Steven <svramirez20@gmail.com>
2021-06-18 12:33:21 -0400

    caesar shift finished

--- src/caesar.js
+++ src/caesar.js
@@ -4,39 +4,54 @@
 // of the anonymous function on line 6
 
 const caesarModule = (function () {
-  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
 
   function caesar(input, shift, encode = true) {
-    //console.log(input, shift);
     if (shift > 25 || shift < -25 || shift === 0) {
       return false;
     }
-    let finalMessage = '';
-    if (encode === true) {
-    for (let i=0; i < input.length; i++) {
-      if (input[i].charCodeAt() > 96) {
-        finalMessage += String.fromCharCode(input[i].charCodeAt() + shift);
-        //must resolve wrap-around issue
-        for (let j=0; j < finalMessage.length; j++) {
-          let letter = finalMessage[j];
-          if (letter.charCodeAt() > 122) {
-            letter.replace(letter.charCodeAt() - 26);
-            console.log(finalMessage);
-            //doesn't work
-          }
-        }
-        }
+    let finalMessage = "";
+    input = input.toLowerCase();
+    if (!encode) shift *= -1;
+    for (let i = 0; i < input.length; i++) {
+      if (input.charCodeAt(i) < 97) {
+        finalMessage += input[i];
+        continue;
+      }
+      let charCode = input.charCodeAt(i) + shift;
+      if (charCode > 122) charCode -= 26;
+      else if (charCode < 97) charCode += 26;
+      finalMessage += String.fromCharCode(charCode);
     }
-        return finalMessage;
-     } else if (encode === false) {
-       console.log(input, shift);
-    for (let i=0; i < input.length; i++) {
-      if (input[i].charCodeAt() > 96) {
-        finalMessage += String.fromCharCode(input[i].charCodeAt() - shift);       
-        }
-    }
+
     return finalMessage;
-     }
   }
   return {
     caesar,
