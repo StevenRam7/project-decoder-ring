294580cb27a8f0cb9a66e9397cb5b924df812884
Steven <svramirez20@gmail.com>
2021-06-18 04:25:08 -0400

    issue w. wrap-around & spaces

--- src/caesar.js
+++ src/caesar.js
@@ -4,26 +4,40 @@
 // of the anonymous function on line 6
 
 const caesarModule = (function () {
-  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
   function caesar(input, shift, encode = true) {
-    console.log(input, shift);
+    //console.log(input, shift);
     if (shift > 25 || shift < -25 || shift === 0) {
       return false;
     }
-    let finalMessage = [];
+    let finalMessage = '';
     if (encode === true) {
     for (let i=0; i < input.length; i++) {
-      if (input[i] === alphabet[i] && input[i].charCodeAt() > 96) {
-        const shifted = input[i].replace(alphabet[i+shift]);
-        finalMessage.push(shifted);
-      };
+      if (input[i].charCodeAt() > 96) {
+        finalMessage += String.fromCharCode(input[i].charCodeAt() + shift);
+        //must resolve wrap-around issue
+        for (let j=0; j < finalMessage.length; j++) {
+          let letter = finalMessage[j];
+          if (letter.charCodeAt() > 122) {
+            letter.replace(letter.charCodeAt() - 26);
+            console.log(finalMessage);
+            //doesn't work
+          }
+        }
+        }
     }
-      return finalMessage;
+        return finalMessage;
+     } else if (encode === false) {
+       console.log(input, shift);
+    for (let i=0; i < input.length; i++) {
+      if (input[i].charCodeAt() > 96) {
+        finalMessage += String.fromCharCode(input[i].charCodeAt() - shift);       
+        }
     }
-    
+    return finalMessage;
+     }
   }
-  
   return {
     caesar,
   };
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,2 +1,10 @@
 const ceasarModule = require("../src/caesar");
 const expect = require("chai").expect;
+
+describe("ceaserModule", () => {
+    it("should perform a Caesar shift while ignoring caps & spaces", () => {
+        const actual = ceasarModule.caesar("a message", 3, encode = true);
+        const expected = 'd phvvdjh';
+        expect(actual).to.equal(expected);
+    })
+})
