08deaaa1073080f4be8318c412b0b108448c328f
Steven <svramirez20@gmail.com>
2021-06-23 21:01:07 -0400

    polybius and substitution partially complete

--- src/polybius.js
+++ src/polybius.js
@@ -4,27 +4,50 @@
 // of the anonymous function on line 6
 
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
+  const square =
+        //add values for grid
 
   function polybius(input, encode = true) {
     input = input.toLowerCase();
     console.log(input);
-    let finalMessage = ' ';
-    const charNum = letter.charCodeAt() - 97;
+    let finalMessage = '';
+    if (encode) {
     for (let i=0; i < input.length; i++) {
       const letter = input[i];
-      if (letter >= 'i') {
+      const charNum = letter.charCodeAt(0) - 97;
+      //97 is charCode for 'a'
+      if (letter == ' ') {
+        finalMessage += ' ';
+        continue;
+      }
+      if (letter.charCodeAt(0) <= 105) {
       let row = Math.floor(charNum/5) + 1;
       let column = charNum%5 + 1;
-      finalMessage += `${row}${column}`;
-        
-      } else if (letter < 'i') {
-      let row = Math.floor(charNum - 1/5) + 1;
-      let column = (charNum - 1)%5 + 1;
-      finalMessage += `${row}${column}`;
+      finalMessage += `${column}${row}`;
+       }
+      else if (letter.charCodeAt(0) > 105) {
+      const newCharNum = letter.charCodeAt(0) - 98;
+      let row = Math.floor(newCharNum/5) + 1;
+      let column = (newCharNum%5) + 1;
+      finalMessage += `${column}${row}`;
+      }
+      
       }
       console.log(finalMessage);
-      console.log(row, column);
+      return finalMessage;
+    }
+    if (!encode) {
+      const pairCount = input.length/2;
+      //
+      console.log(input);
+      for (let i=0; i < input.length; i+=2) {
+        let adjustedInput = '';
+        adjustedInput += `${input[i]}${input[i+1]} `;
+        //adjustedInput[0] = column, adjustedInput[1] = row
+        
+        console.log(adjustedInput);
+    }
+  }
   }
 
   return {
--- src/substitution.js
+++ src/substitution.js
@@ -7,7 +7,43 @@ const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    let finalMessage = '';
+    console.log(input, alphabet)
+     if (!alphabet) {
+      return false;
+    }
+    if (alphabet.length != 26) {
+      return false;
+    }
+    if (alphabet.length === 26) {
+      const mirrorAlphabet = alphabet;
+      console.log(alphabet, mirrorAlphabet);
+    }
+    if (encode) {
+    for (let i=0; i < input.length; i++) {
+      if (input[i] == ' ') {
+        finalMessage += ' ';
+        continue;
+      }
+      const letter = input.charCodeAt(i) - 97;
+      finalMessage += alphabet[letter];
+    }
+      console.log(finalMessage);
+      return finalMessage;
+  }
+    
+    if (!encode) {
+      for (let i=0; i < input.length; i++) {
+      if (input[i] == ' ') {
+        finalMessage += ' ';
+        continue;
+      }
+        const letter = input[i];
+        finalMessage += String.fromCharCode(letter + 97);
+    }
+      console.log(finalMessage);
+      return finalMessage;
+  }
   }
 
   return {
